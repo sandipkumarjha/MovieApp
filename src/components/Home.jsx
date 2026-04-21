4be635f2436eecaf0d72a945852e14fc3f2dc278
Sandip kumar jha <sandipkumarjha211@gmail.com>
2026-04-22 01:11:15 +0530

    Home and cards completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,11 +4,12 @@ import TopNav from './templates/TopNav'
 import axios from '../utils/axios'
 import Header from './templates/Header'
 import HorizontalCards from './templates/HorizontalCards'
-
+import Dropdown from './templates/Dropdown'
 const Home = () => {
     document.title = 'Home - Movie App'
     const [wallpaper,setwallpaper] = useState(null);
-    
+    const [trending,settrending] = useState(null);
+    const [category,setcategory]   = useState("all");
     const GetHeaderwallpaper = async () => {
       try {
         const { data } = await axios.get(`/trending/all/day`);
@@ -22,17 +23,35 @@ const Home = () => {
         console.log(err);
       }
     };
+    const GetTrending = async () => {
+      try {
+        const { data } = await axios.get(`/trending/${category}/day`);
+        
+
+        settrending(data.results);
+
+      } catch (err) { 
+        console.log(err);
+      }
+    };
      useEffect(() => {
       !wallpaper && GetHeaderwallpaper();
-    }, []);
+      GetTrending();
+    }, [category]);
 
-  return  wallpaper?(
+  return  wallpaper && trending ?(
     <>
     <Sidenav/> 
-    <div className='w-[80%] h-fulL  overflow -auto overflow-x-hidden   '>
+    <div className='w-[80%] h-fulL  overflow-auto overflow-x-hidden   '>
         <TopNav/>
         <Header  data = {wallpaper}/>
-        <HorizontalCards />
+        <div className=' flex justify-between mt-3 p-10'>
+        <h1 className='text-white mb-5 text-3xl font-semibold '>
+            Trending
+        </h1>
+        <Dropdown title = "Filter" options ={['tv','movie','all']} func ={(e)=> setcategory(e.target.value)}/>
+        </div>
+        <HorizontalCards  data = {trending} func  = {setcategory}/>
     </div>
 
     
--- src/components/templates/HorizontalCards.jsx
+++ src/components/templates/HorizontalCards.jsx
@@ -1,19 +1,39 @@
 import React from 'react'
+import Dropdown from './Dropdown'
 
-const HorizontalCards = () => {
+const HorizontalCards = ({data}) => {
   return (
-    <div className='w-full h-[40vh] p-5  '>
-        <div className='mb-5'>
-        <h1 className='text-white mb-5 text-3xl font-semibold '>
-            Trending
-        </h1>
+
+        
+        <div className='w-full flex  overflow-x-auto  gap-5 p-5'>
+       {data.map((item,index) => (
+       <div key={index} className='min-w-[15%] bg-zinc-900 rounded-lg mt-5 mb-5'>
+        <img className='w-full h-[20vh] object-cover rounded-lg' 
+        src={`https://image.tmdb.org/t/p/original/${
+          item?.backdrop_path || item?.poster_path
+        })`} alt="" />
+        <div className='p-3 h-[55%]'>
+        <h1 className="w-[70%] text-xl text-white font-semibold">
+        {item?.name ||
+          item?.title ||
+          item?.original_name || 
+          item?.original_title}
+      </h1>
+      
+
+      <p className="text-white font-serif mt-3">
+        {item.overview.slice(0, 70)}......
+        <span className="text-zinc-500">more</span>
+      </p>
         </div>
+        
+       </div>))}
 
 
 
 
-        <div className='w-[15%] h-full bg-amber-50  '></div>
-    </div>
+        </div>
+    
   )
 }
 
