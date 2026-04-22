beb5757b88c0969c36612c13e6ace7978f736844
Sandip kumar jha <sandipkumarjha211@gmail.com>
2026-04-22 23:52:13 +0530

    Fix on Trending,Drpdown&cards

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -2,27 +2,54 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import Topnav from './templates/Topnav';
 import Dropdown from './templates/Dropdown';
+import axios from '../utils/axios';
+import { useEffect, useState } from 'react';
+import Cards from './templates/Cards';
+import Loading from './Loading';
 
 const Trending = () => {
     const navigate = useNavigate();
-  return (
-    <div className='p-[3%] w-screen h-screen'>
+    const [Category, setCategory] = useState("all");
+    const [duration, setduration] = useState("day");
+    const [trending, settrending] = useState(null);
+
+    const GetTrending = async () => {
+        try {
+          const { data } = await axios.get(`/trending/${Category}/${duration}`);
+          
+  
+          settrending(data.results);
+  
+        } catch (err) { 
+          console.log(err);
+        }
+      };
+         useEffect(() => {
+          GetTrending();
+        }, [Category , duration]);
+
+  return trending ? (
+    <div className='px-[3%] w-screen h-screen overflow-hidden overflow-y-auto'>
 
         <div className=' w-full  flex items-center '>
            
-            <h1 className='text-3xl font-semibold text-zinc-400'>
+            <h1 className='w-[20%] text-3xl font-semibold text-zinc-400'>
                 
             <i onClick={() =>navigate(-1)} 
-            class="hover:text-[#6556CD] ri-arrow-left-fill"></i> Trending</h1>
+            className="hover:text-[#6556CD] ri-arrow-left-fill"></i> Trending</h1>
             <Topnav />
             <Dropdown 
             title= 'Category' 
             options={["movie", "tv", "all"]} 
-             func=""
+             func= {(e) => setCategory(e.target.value)}
             />
+            <div className='w-[2%]'></div>
+            <Dropdown title='Duration' options={["week" , "day"]} func="" />
         </div>
-      
+      <Cards data={trending} title ={Category}/>
     </div>
+  ) :(
+    <Loading />
   )
 }
 
